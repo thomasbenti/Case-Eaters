d818e4d6f3cf8a8cd4e52b4dbdaba268833b6b14
k-borkowski221 <katebork22@gmail.com>
2025-10-13 18:46:07 -0400

    finished out login verification route and account addition route. next step is to integrate this with front end. using post /users for account registration and push /users/login for login verification

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+
 app.use(express.json())
 
 const users = []
@@ -8,10 +10,32 @@ app.get('/users', (req, res) => {
   res.json(users)
 })
 
-app.post('/users', (req, res) => {
-  const user = {username: req.body.username, password: req.body.password}
-  users.push(user)
-  res.status(201).send()
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        console.log(hashedPassword)
+        const user = {username: req.body.username, password: hashedPassword}
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.username === req.body.username)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
 })
 
 app.listen(3000)
