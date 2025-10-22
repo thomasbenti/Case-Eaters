7ac5620e5f381932f38bbd55c0064c33b94c247b
Angel Diaz <sad123@case.edu>
2025-10-22 16:38:47 -0400

    Add Jest testing framework and GitHub Actions CI workflow

---
+++ backend/tests/user.test.js
@@ -0,0 +1,10 @@
+const User = require('../models/User');
+
+describe('User Model', () => {
+  test('is invalid if username or password missing', () => {
+    const user = new User();
+    const err = user.validateSync();
+    expect(err.errors).toHaveProperty('username');
+    expect(err.errors).toHaveProperty('password');
+  });
+});
