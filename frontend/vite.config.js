757ba75ca272b712620f89f077dccb8eefa9a462
k-borkowski221 <katebork22@gmail.com>
2025-12-05 21:10:07 -0500

    updating stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // backend port
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
 });
